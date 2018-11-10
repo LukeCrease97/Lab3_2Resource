84ff1299a50d8bc69126a83a10636513c90c06ce
LukeCrease97 <hengyh-wa16@student.tarc.edu.my>
2018-11-10 16:06:52 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tar/lab3_2resource/MainActivity.java
@@ -0,0 +1,30 @@
+package my.edu.tar.lab3_2resource;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewMessage = findViewById(R.id.textViewMsg);
+
+    }
+
+    public void showMessage(View view)
+    {
+        textViewMessage.setText(getString(R.string.message));
+
+
+    }
+
+}
